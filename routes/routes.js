cdeccc6cda6074279ffd1a21b0287d3fb586d79b
Ryan Henness <ryanhenness@gmail.com>
2017-02-06 21:04:22 -0800

    Added examples: login, login check, routes, ejs rendering...

--- routes/routes.js
+++ routes/routes.js
@@ -10,4 +10,16 @@ module.exports = function(app){
 	app.get('/', function(req, res){
     res.render('index', {user: req.user});
   });
+
+  app.get('/example', loggedIn, function(req, res){
+    res.render('example', {user: req.user});
+  });
+
+  function loggedIn(req, res, next) {
+    if (req.user) {
+        next();
+    } else {
+        res.redirect('/');
+    }
+  }
 }
