aa9800bddfc88109f458502aeaf7f7a1b211dd74
Ryan Henness <ryanhenness@gmail.com>
2017-02-07 16:38:46 -0800

    Added db user input, and registration check

--- routes/routes.js
+++ routes/routes.js
@@ -7,19 +7,24 @@ module.exports = function(app){
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false}));
 
-	app.get('/', function(req, res){
-    res.render('index', {user: req.user});
+	app.get('/', loggedIn, function(req, res){
+    // Adds user entry to user table no matter what..
+    // We'll need to check whether the user is registered or not in pg.js
+    pg.CheckRegistration(req.user.id, callback);
+    function callback(){
+      res.render('index', {user: req.user});
+    }
   });
 
-  app.get('/example', loggedIn, function(req, res){
-    res.render('example', {user: req.user});
+  app.get('/login', function(req, res){
+    res.render('login', {user: req.user});
   });
 
   function loggedIn(req, res, next) {
     if (req.user) {
         next();
     } else {
-        res.redirect('/');
+        res.redirect('/login');
     }
   }
 }
