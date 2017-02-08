b49e735b0a51ff91990151897c74bf6c6414b6ec
Ryan Henness <ryanhenness@gmail.com>
2017-02-07 16:44:18 -0800

    Removed AddUser, added code to CheckRegistration

--- routes/pg.js
+++ routes/pg.js
@@ -8,26 +8,14 @@ module.exports = function(app){
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false}));
 
-  // Adds a user to the user table, inserting their Facebook ID
-  var AddUser = function AddUser(fbid, callback){
-    pg.connect(connect, function(err, client, done){
-        client.query("INSERT INTO public.user (fbid) VALUES ($1)", [fbid], function(err, result){
-            console.log('User added to database with id: ' + id);
-            done(); // Called when connection to database is finished...
-            if(callback){
-              callback(); // To continue code called in parent function
-            }
-        }); 
-    });
-  };
-  module.exports.AddUser = AddUser;
-
   // Check whether a user is registered in database. If not, add them.
   var CheckRegistration = function CheckRegistration(fbid, callback){
     pg.connect(connect, function(err, client, done){
         client.query("SELECT * FROM public.user WHERE fbid=$1", [fbid], function(err, result){
             if(result.rows.length == 0){
-              AddUser(id);
+              client.query("INSERT INTO public.user (fbid) VALUES ($1)", [fbid], function(err, result){
+                console.log('User added to database with id: ' + fbid);
+              }); 
             }
             done(); // Called when connection to database is finished...
             callback(); // To continue code called in routes.js
