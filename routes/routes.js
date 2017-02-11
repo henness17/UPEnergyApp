19d985287165cedece2363a649999eaca3a38fbc
Ryan Henness <ryanhenness@gmail.com>
2017-02-10 23:53:08 -0800

    Added navbar views

--- routes/routes.js
+++ routes/routes.js
@@ -12,12 +12,24 @@ module.exports = function(app){
     // We'll need to check whether the user is registered or not in pg.js
     pg.CheckRegistration(req.user.id, callback);
     function callback(){
-      res.render('index', {user: req.user});
+      res.render('home', {user: req.user});
     }
   });
 
   app.get('/login', function(req, res){
     res.render('login', {user: req.user});
+  });
+
+  app.get('/navigation', function(req, res){
+    res.render('navigation', {user: req.user});
+  });
+
+  app.get('/social', function(req, res){
+    res.render('social', {user: req.user});
+  });
+
+  app.get('/settings', function(req, res){
+    res.render('settings', {user: req.user});
   });
 
   function loggedIn(req, res, next) {
