157f93dd5e6cc13e0554123c46bbb2b2b33357c0
Ryan Henness <ryanhenness@gmail.com>
2017-02-18 14:46:45 -0800

    Transportation's kwPerMile mockup added

--- routes/calculators/transportation.js
+++ routes/calculators/transportation.js
@@ -13,4 +13,20 @@ module.exports = function(app){
     }
   };
   module.exports.GetMpg = GetMpg;
+
+  var GetKwPerMiPersonalVehicle = function GetKwPerMiPersonalVehicle(car_class){
+  	var kW_conversion = 0.000293;
+  	if (car_class === "SUV"){
+   		return 5472*kW_conversion;
+   	}else if(car_class === "Passenger"){
+   		return 2989*kW_conversion;
+   	}else if(car_class === "Van"){
+   		return 5472*kW_conversion;
+   	}else if(car_class === "Pickup"){
+   		return 5472*kW_conversion;
+    }else if(car_class === "Motocycle"){
+    	return 2665*kW_conversion;
+    }
+  };
+  module.exports.GetKwPerMiPersonalVehicle = GetKwPerMiPersonalVehicle;
 };
